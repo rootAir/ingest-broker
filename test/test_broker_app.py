cfbc9bcdb9ccd87388f470022e736c234888bf1c
Rodrey Mark Goite <rodrey@ebi.ac.uk>
2018-06-06 15:59:18 +0100

    Removed outdated tests.

--- test/test_broker_app.py
+++ test/test_broker_app.py
@@ -1,12 +1,8 @@
 from unittest import TestCase
-
-from flask import json
 from unittest.mock import patch
 
 from broker import broker_app
 from broker.broker_app import app
-from ingest.importer.hcaxlsbroker import SpreadsheetSubmission
-from ingest.api.ingestapi import IngestApi
 
 
 class BrokerAppTest(TestCase):
@@ -32,63 +28,3 @@ class BrokerAppTest(TestCase):
 
             # then:
             self.assertEqual(500, response.status_code)
-
-    def test_value_error_on_spreadsheet_submission(self):
-        with patch.object(broker_app, '_save_file') as save_file, \
-                patch.object(SpreadsheetSubmission, 'submit') as submit_spreadsheet:
-            # given:
-            save_file.return_value = 'path/to/file.xls'
-            submit_spreadsheet.side_effect = ValueError("value error")
-
-            # when:
-            response = self.client.post('/api_upload', headers={'Authorization': 'auth'})
-
-            # then:
-            self.assertEqual(400, response.status_code)
-
-    def test_key_error_on_spreadsheet_submission(self):
-        with patch.object(broker_app, '_save_file') as save_file, patch.object(SpreadsheetSubmission,
-                                                                               'submit') as submit_spreadsheet:
-            # given:
-            save_file.return_value = 'path/to/file.xls'
-            submit_spreadsheet.side_effect = KeyError("key error")
-
-            # when:
-            response = self.client.post('/api_upload', headers={'Authorization': 'auth'})
-
-            # then:
-            self.assertEqual(400, response.status_code)
-
-    def test_submission_successful(self):
-        with patch.object(broker_app, '_save_file') as save_file, \
-                patch.object(SpreadsheetSubmission, 'submit') as submit_spreadsheet, patch.object(SpreadsheetSubmission,
-                                                                                                  'createSubmission') as create_submission, \
-                patch.object(IngestApi, '__init__') as init_ingest_api, \
-                patch.object(IngestApi, 'getObjectUuid') as get_object_uuid:
-            # given:
-            save_file.return_value = 'path/to/file.xls'
-
-            # and:
-            submission_id = '63fde9'
-            submission_url = "https://sample.com/submission/%s" % submission_id
-            create_submission.return_value = submission_url
-
-            # given:
-            init_ingest_api.return_value = None
-            uuid = 'dc54c78c-8556-4750-b527-b35b26645e39'
-            get_object_uuid.return_value = uuid
-
-            # when:
-            response = self.client.post('/api_upload', headers={'Authorization': 'auth'})
-
-            # then:
-            self.assertEqual(201, response.status_code)
-
-            # and: assert correct JSON details
-            self.assertTrue(response.data)
-            response_json = json.loads(response.data)
-            response_details = response_json['details']
-            self.assertEqual(submission_url, response_details['submission_url'])
-            self.assertEqual(uuid, response_details['submission_uuid'])
-            self.assertEqual(uuid, response_details['display_uuid'])
-            self.assertEqual(submission_id, response_details['submission_id'])
