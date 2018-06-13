d13a2652e25cd8c81a7a626a9ed01168d2ce3575
Rolando Fernandez <rolando@ebi.ac.uk>
2018-06-13 15:15:25 +0100

    test for generating a submission summary

--- test/service/test_summary_service.py
+++ test/service/test_summary_service.py
@@ -21,6 +21,47 @@ class SummaryServiceTest(TestCase):
             assert mock_specific_type in entity_summary.breakdown
             assert entity_summary.count == 10
 
+    def test_generate_submission_summary(self):
+        mock_ingest_api = patch('__main__.IngestApi')
+        mock_envelope_uri = 'http://mock-ingest-api/envelopes/mock-envelope-id'
+
+        summary_service = SummaryService(mock_ingest_api)
+        with patch('broker.service.summary_service.SummaryService.get_entities_in_submission') as mock_get_entities_in_submission:
+            def get_entities_in_submission_mock(*args, **kwargs):
+                envelope_uri = args[0]
+                entity_type = args[1]
+
+                type_to_mock_specific_type_map = {'processes': 'specific-process',
+                                                  'biomaterials': 'specific-biomaterial',
+                                                  'protocols': 'specific-protocol',
+                                                  'files': 'specific-file',
+                                                  'projects': 'specific-project'
+                                                  }
+
+                if envelope_uri == mock_envelope_uri:
+                    yield from self.generate_mock_entities(10, type_to_mock_specific_type_map[entity_type])
+                else:
+                    yield
+
+            mock_get_entities_in_submission.side_effect = get_entities_in_submission_mock
+            submission_summary = summary_service.summary_for_submission(mock_envelope_uri)
+            
+            assert submission_summary.process_summary.count == 10
+            assert 'specific-process' in submission_summary.process_summary.breakdown
+
+            assert submission_summary.file_summary.count == 10
+            assert 'specific-file' in submission_summary.file_summary.breakdown
+
+            assert submission_summary.biomaterial_summary.count == 10
+            assert 'specific-biomaterial' in submission_summary.biomaterial_summary.breakdown
+
+            assert submission_summary.project_summary.count == 10
+            assert 'specific-project' in submission_summary.project_summary.breakdown
+
+            assert submission_summary.protocol_summary.count == 10
+            assert 'specific-protocol' in submission_summary.protocol_summary.breakdown
+
     @staticmethod
     def generate_mock_entities(count, specific_type):
-        return list(map(lambda indx: {'content': {'describedBy': 'http://mock-schema/something/{0}'.format(specific_type)}}, range(0, count)))
+        for i in range(0, count):
+            yield {'content': {'describedBy': 'http://mock-schema/something/{0}'.format(specific_type)}}
