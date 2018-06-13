9b8b0b625976a971d849207f663f9ef95ced2175
Rolando Fernandez <rolando@ebi.ac.uk>
2018-06-13 17:13:34 +0100

    page size of 1000 when retrieving the entities in a submission to reduce # of network calls

--- broker/service/summary_service.py
+++ broker/service/summary_service.py
@@ -48,7 +48,7 @@ class SummaryService:
         return self.generate_summary_for_entity(submission_uri, 'processes')
 
     def get_entities_in_submission(self, submission_uri, entity_type) -> Generator[dict, None, None]:
-        yield from self.ingestapi.getEntities(submission_uri, entity_type)
+        yield from self.ingestapi.getEntities(submission_uri, entity_type, 1000)
 
     def generate_summary_for_entity(self, submission_uri, entity_type) -> SubmissionSummary.EntitySummary:
         """
