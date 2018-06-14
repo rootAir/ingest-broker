eb769faeb93fcb73d9dee562cfdd9597e61fabf5
Rolando Fernandez <rolando@ebi.ac.uk>
2018-06-14 11:44:57 +0100

    added method for adding EntitySummary together

--- broker/common/entity_summary.py
+++ broker/common/entity_summary.py
@@ -2,3 +2,18 @@ class EntitySummary:
     def __init__(self):
         self.count = 0
         self.breakdown = dict()
+
+    def __add__(self, other: 'EntitySummary') -> 'EntitySummary':
+        """
+        adds two EntitySummary together
+        :param other:
+        :return: self
+        """
+        self.count += other.count
+        for (key, val) in other.breakdown.items():
+            if key in self.breakdown:  # ..do an add
+                self.breakdown[key]['count'] += val['count']
+            else:
+                self.breakdown[key] = val
+
+        return self
--- broker/common/project_summary.py
+++ broker/common/project_summary.py
@@ -1,5 +1,6 @@
 from .entity_summary import EntitySummary
 
+
 class ProjectSummary:
 
     def __init__(self):
@@ -12,3 +13,11 @@ class ProjectSummary:
         self.submission_status = None
         self.create_date = None
         self.last_updated_date = None
+
+    def addSubmissionSummary(self, submission_summary) -> 'ProjectSummary':
+        """
+        Adds a submission summary to this project summary
+        :param submission_summary: SubmissionSummary to add to self
+        :return: self
+        """
+        return self
