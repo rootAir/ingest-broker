581126b09446ec8e41471b80327eea2a84c18992
Rolando Fernandez <rolando@ebi.ac.uk>
2018-06-14 12:56:38 +0100

    tests for adding a SubmissionSummary to a ProjectSummary

--- broker/common/project_summary.py
+++ broker/common/project_summary.py
@@ -1,4 +1,5 @@
 from .entity_summary import EntitySummary
+from .submission_summary import SubmissionSummary
 
 
 class ProjectSummary:
@@ -8,16 +9,20 @@ class ProjectSummary:
         self.protocol_summary = EntitySummary()
         self.process_summary = EntitySummary()
         self.file_summary = EntitySummary()
-        self.project_summary = EntitySummary()
 
         self.submission_status = None
         self.create_date = None
         self.last_updated_date = None
 
-    def addSubmissionSummary(self, submission_summary) -> 'ProjectSummary':
+    def addSubmissionSummary(self, submission_summary: 'SubmissionSummary') -> 'ProjectSummary':
         """
         Adds a submission summary to this project summary
         :param submission_summary: SubmissionSummary to add to self
         :return: self
         """
+        self.biomaterial_summary += submission_summary.biomaterial_summary
+        self.protocol_summary += submission_summary.protocol_summary
+        self.process_summary += submission_summary.process_summary
+        self.file_summary += submission_summary.file_summary
+
         return self
