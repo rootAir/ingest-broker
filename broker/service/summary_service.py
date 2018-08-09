6682c15cd7562707497c9ee462b3d8bb4a9c10d5
Rolando Fernandez <rolando@ebi.ac.uk>
2018-08-09 15:08:07 +0100

    added constructs for representing submission scrape directives

--- broker/service/summary_service.py
+++ broker/service/summary_service.py
@@ -7,6 +7,11 @@ from .exception.cache_miss_exception import CacheMissException
 
 
 from typing import Generator
+from typing import Iterable
+from typing import Callable
+from functools import reduce
+
+import json
 
 
 class SummaryService:
@@ -23,6 +28,7 @@ class SummaryService:
             self.processes = processes if processes else []
             self.protocols = protocols if protocols else []
             self.files = files if files else []
+
 
     def summary_for_project(self, project_resource) -> ProjectSummary:
         project_summary = ProjectSummary()
@@ -59,6 +65,7 @@ class SummaryService:
 
             submissions_entities = self.get_all_entities_in_submission(submission_uri)
             submission_summary = self.add_entity_count_breakdown(submission_summary, submissions_entities)
+            self.add_specific_submission_information(submission_summary, submissions_entities, None) # TODO: pass a scrape-config
 
             submission_summary.create_date = submission_resource['submissionDate']
             submission_summary.last_updated_date = submission_resource['updateDate']
@@ -74,6 +81,21 @@ class SummaryService:
         submission_summary.file_summary = self.generate_summary_for_entity(submission_entities.files)
         submission_summary.process_summary = self.generate_summary_for_entity(submission_entities.processes)
         return submission_summary
+
+
+    def add_specific_submission_information(self, submission_summary, submission_entities, information_map):
+        """
+        With instructions from the information_map, this will attempt to find specific submission info and add
+        it to the summary
+        :param submission_summary:
+        :param submission_entities:
+        :param information_map:
+        :return:
+        """
+
+        pass # TODO
+
+
 
     def get_entities_in_submission(self, submission_uri, entity_type) -> Generator[dict, None, None]:
         yield from self.ingestapi.getEntities(submission_uri, entity_type, 1000)
@@ -129,4 +151,69 @@ class SummaryService:
     @staticmethod
     def uuid_from_submission(submission_resource) -> str:
         return submission_resource['uuid']['uuid']
+
+class ScrapeConfig:
+    """
+    Represents a configuration file used to configure a SubmissionScraper
+    """
+
+    def __init__(self, placeholder_name, entity_type, path, post_process):
+        self.placeholder_name = placeholder_name
+        self.entity_type = entity_type
+        self.path = path
+        self.post_process = post_process
+
+class SubmissionScraper:
+
+
+    @staticmethod
+    def scrape_configs_from_dict(config: dict) -> Generator[ScrapeConfig, None, None]:
+        yield from map(lambda conf: ScrapeConfig(conf['placeholder'],
+                                                 conf['entity_type'],
+                                                 conf['path'],
+                                                 conf['post_process']),
+                       config.items())
+
+    @staticmethod
+    def scrape_configs_from_file(file) -> Generator[ScrapeConfig, None, None]:
+        with open(file, 'rb') as config_file:
+            config = json.load(config_file)
+            yield from SubmissionScraper.scrape_configs_from_dict(config)
+
+    class Directive:
+        """
+        A directive will instruct the scraper on where and how to find info to be scraped. Can be constructed
+        with an optional reducer function to combine matching scraped data. The reducer function should take a
+        collection of matching field-values as param and return a single value
+        """
+
+        def __init__(self, placeholder=None, entity_type=None, field_path=None, reducer=None):
+            self.placeholder = placeholder
+            self.entity_type = entity_type
+            self.field_path = field_path
+            self.reducer = reducer
+
+    def __init__(self, directives=None):
+        self.directives = directives if directives else []
+
+    @staticmethod
+    def from_scrape_configs(scrape_configs: Generator[ScrapeConfig, None, None]) -> Iterable[Directive]:
+        """
+        returns directives given scrap configs
+        :param scrape_configs:
+        :return:
+        """
+        return map(lambda scrape_config: SubmissionScraper.Directive(scrape_config.placeholder,
+                                                                     scrape_config.entity_type,
+                                                                     scrape_config.path,
+                                                                     SubmissionScraper.reducer_for(scrape_config.post_process)), scrape_configs)
+
+    @staticmethod
+    def reducer_for(post_process_func: str):
+        if post_process_func == "sum":
+            return lambda collection: reduce((lambda x,y: x + y), collection)
+        elif post_process_func == "count":
+            return lambda collection: reduce((lambda x,y: x + 1), collection, 0)
+        else:
+            return lambda collection: collection # just return the collection
 
